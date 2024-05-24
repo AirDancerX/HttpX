ab73d5cb1d6a47f681a64b2d1be95f45b76a7d29
AirDancerX <441692558@qq.com>
2024-05-24 13:15:50 +0800

    Delete src/test/java/com/example/ExamplePluginTest.java

--- src/test/java/com/HttpX/ServerPluginTest.java
+++ src/test/java/com/HttpX/ServerPluginTest.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.HttpX;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ServerPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
